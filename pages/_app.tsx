c2cc0882416206396d76ffa84c88986bb8b0b525
borabiricik <bora.bir496@gmail.com>
2023-04-14 22:46:10 +0300

    feat: Redux toolkit init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
+import { store } from "@/core/redux";
 import { avant, avenir } from "@/fonts/index";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className={`${avant.variable} ${avenir.variable}`}>
-      <Component {...pageProps} />
-    </div>
+    <Provider store={store}>
+      <div className={`${avant.variable} ${avenir.variable}`}>
+        <Component {...pageProps} />
+      </div>
+    </Provider>
   );
 }
 
