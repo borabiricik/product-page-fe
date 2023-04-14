1e84effc06102fd7d169e3c460563c9052103bfb
borabiricik <bora.bir496@gmail.com>
2023-04-14 23:13:39 +0300

    feat(layout): Layout overflow scroll handle

--- src/modules/common/layouts/PageLayout/index.tsx
+++ src/modules/common/layouts/PageLayout/index.tsx
@@ -2,9 +2,9 @@ import { IPageLayoutProps } from "./types";
 
 const PageLayout = ({ children, ...props }: IPageLayoutProps) => {
   return (
-    <div className="flex items-stretch h-screen">
-      <aside className="flex-1 bg-blue-200 w-full">asd</aside>
-      <main className="flex-1 flex items-center justify-center bg-red-200">
+    <div className="flex items-stretch h-screen overflow-y-scroll">
+      <aside className="flex-1 bg-blue-200 w-full sticky top-0">asd</aside>
+      <main className="flex-1 flex items-center justify-center bg-red-200 h-[1000px]">
         {children}
       </main>
     </div>
