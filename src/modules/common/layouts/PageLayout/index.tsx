4e66d9bdd52ed055021f481763cddf666026be84
borabiricik <bora.bir496@gmail.com>
2023-04-14 23:37:25 +0300

    feat(layout): Layout left sided image added

--- src/modules/common/layouts/PageLayout/index.tsx
+++ src/modules/common/layouts/PageLayout/index.tsx
@@ -1,9 +1,20 @@
+import Image from "next/image";
 import { IPageLayoutProps } from "./types";
 
 const PageLayout = ({ children, ...props }: IPageLayoutProps) => {
   return (
     <div className="flex items-stretch h-screen overflow-y-scroll">
-      <aside className="flex-1 bg-blue-200 w-full sticky top-0">asd</aside>
+      <aside className="flex-1 bg-blue-200 w-full sticky top-0">
+        <Image
+          width={998}
+          height={1198}
+          src={"/images/bg.png"}
+          alt="Layout left image"
+          className="object-cover w-full h-full"
+          quality={100}
+          priority
+        />
+      </aside>
       <main className="flex-1 flex items-center justify-center bg-red-200 h-[1000px]">
         {children}
       </main>
